71cb14c5d217e817fa3b7d64626e4708f727f5a0
michaelcoronado <michaelcoronado1@hotmail.es>
2015-11-10 23:48:42 -0500

    Integrador II

---
+++ Assets/INICIO.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class INICIO : MonoBehaviour {
+    public void Inicio(string level)
+    {
+        Application.LoadLevel("INICIO");
+    }
+    public void SHURE(string level)
+    {
+        Application.LoadLevel("SHURE");
+    }
+
+    public void AKG(string level)
+    {
+        Application.LoadLevel("AKG");
+    }
+    public void FREAKG(string level)
+    {
+        Application.LoadLevel("FREAKG");
+    }
+    public void FRESHURE(string level)
+    {
+        Application.LoadLevel("FRESHURE");
+    }
+    public void POLARSHURE(string level)
+    {
+        Application.LoadLevel("POLARSHURE");
+    }
+    public void POLARAKG(string level)
+    {
+        Application.LoadLevel("POLARAKG");
+    }
+}
